c40cb72b74254464f2cf9a86aa4c16061dcec62f
Michael Bradley, Jr. <michaelsbradleyjr@gmail.com>
2023-10-01 15:57:06 -0500

    [wip] experiment with raw c wrapper in examples/cozo1

--- examples/cozo1.nim
+++ examples/cozo1.nim
@@ -1,6 +1,30 @@
 {.push raises: [].}
 
-import pkg/cozo
+import std/os
+import pkg/cozo/abi
 
-echo cozo()
-echo cozoImpl()
+const
+  # engine = "sqlite"
+  engine = "rocksdb"
+  path = "data" / "cozo1.db"
+  options = "{}"
+
+var dbId: int32
+
+createDir path.parentDir
+
+let resOpenCstr = cozo_open_db(
+  engine.cstring, path.cstring, options.cstring, addr dbId)
+
+var resOpen = "success"
+
+if not resOpenCstr.isNil:
+  resOpen = $resOpenCstr
+  cozo_free_str resOpenCstr
+
+echo resOpen
+echo dbId
+
+let resClose = cozo_close_db dbId
+
+echo resClose
